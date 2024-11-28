e4410b83cb5737b86e5b89430037f1a05c1dfaf3
yassir346 <yassir.mahir98@gmail.com>
2024-11-28 13:56:20 +0100

    adding scroll and the cards data organization

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,3 +1,4 @@
+// générer les données à travers fichier json
 async function fetchPlayers() {
   let dataFromJson = await fetch("../players.json");
   let json = await dataFromJson.json();
@@ -6,7 +7,9 @@ async function fetchPlayers() {
   displayData(players);
 }
 
+fetchPlayers(); //Appeller la fonction.
 
+// la fonction de l'implémentation des données dans l'HTML
 const playersContainer = document.getElementById('players-container');
 
 function displayData(players) {
@@ -14,15 +17,49 @@ function displayData(players) {
       const div = document.createElement('div');
       div.classList.add('player-card');
       div.innerHTML = `
+      <div class="photo-name">
       <img src="
-      ${player.photo}"/>
+      ${player.photo}"/ >
       <p>${player.name}</p>
-      
+      </div>
+      <div class="nat-flag-club">
+      <p>${player.nationality}</p>
+      <img src="${player.flag}"/>
+      <p>${player.club}</p>
+      </div>
+      <img src="${player.logo}"/>
+      <p>${player.rating}</p>
+      <div class="rating-numbers"
+      <p>PAC ${player.pace}</p>
+      <p>SHO ${player.shooting}</p>
+      <p>PAS ${player.passing}</p>
+      <p>DRI ${player.dribbling}</p>
+      <p>DEF ${player.defending}</p>
+      <p>PHY ${player.physical}</p>
+      </div>      
       `;
       playersContainer.appendChild(div);
     });
 }
 
-fetchPlayers();
 
 
+const playerCards = document.querySelectorAll('.playerCard');
+
+playerCards.forEach(element => {
+  
+  element.addEventListener("click", (e) => {
+    console.log(e.target.parentElement.id);
+        
+    })
+});
+
+
+
+player-card.forEach(element => {
+  
+  element.addEventListener("click", (e) => {
+    console.log(e.target.parentElement.id);
+        
+    })
+});
